9902a08883b5b7653378ad565bb25b874b5b1c21
snowykte0426 <snowykte0426@naver.com>
2025-02-08 03:02:00 +0900

    add: AspectJ 프록시 설정 클래스 추가
    AOP의 정상적인 동작을 위한 설정을 적용하였습니다

---
+++ src/main/java/com/ampersand/groom/global/config/AspectJConfig.java
@@ -0,0 +1,9 @@
+package com.ampersand.groom.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AspectJConfig {
+}
