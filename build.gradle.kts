8c3d2d95dd392de00c3b31201d2e58df0e657d03
snowykte0426 <snowykte0426@naver.com>
2025-02-08 03:00:46 +0900

    update: 종속성 추가 및 버전 지정
    ``JJWT``,``QueryDSL``,``MariaDB Java Client``,``Spring Actuator`` 등의 종속성을 추가하고 기존 종속성들에도 버전을 지정하였습니다

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,18 +9,17 @@ version = "0.0.1-SNAPSHOT"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
+		languageVersion.set(JavaLanguageVersion.of(21))
 	}
 }
 
-configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+configurations.named("compileOnly") {
+	extendsFrom(configurations.getByName("annotationProcessor"))
 }
 
 repositories {
 	mavenCentral()
+	maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
@@ -29,23 +28,44 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-mail")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 
 	/** AOP */
-	implementation("
-	implementation("org.springframework.boot:spring-boot-starter-aop")
+	implementation("org.aspectj:aspectjweaver:1.9.19")
 
-    /** Lombok */
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
+	/** Lombok */
+	compileOnly("org.projectlombok:lombok:1.18.36")
+	annotationProcessor("org.projectlombok:lombok:1.18.36")
+
+	/** JWT */
+	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+	implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
+	implementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
 
 	/** Database */
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
-	runtimeOnly("com.mysql:mysql-connector-j")
+	runtimeOnly("com.mysql:mysql-connector-j:9.2.0")
+	runtimeOnly("org.mariadb.jdbc:mariadb-java-client:3.5.1")
 
-	/** Security */
+	/** QueryDSL */
+	implementation("com.querydsl:querydsl-jpa:5.1.0:jakarta")
+	annotationProcessor("com.querydsl:querydsl-apt:5.1.0:jakarta")
+
+	/** Jakarta */
+	implementation("jakarta.persistence:jakarta.persistence-api")
+	implementation("jakarta.transaction:jakarta.transaction-api")
+	implementation("jakarta.validation:jakarta.validation-api")
+	annotationProcessor("jakarta.persistence:jakarta.persistence-api")
+
+	/** Test */
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testImplementation("org.mockito:mockito-core:5.10.0")
+	testImplementation("org.assertj:assertj-core:3.25.3")
+
+	/** Monitoring */
+	implementation("io.micrometer:micrometer-registry-prometheus:1.14.3")
 }
 
 tasks.withType<Test> {
