a1a24609f4d53e3c92a6998862bf6d8f60eb7a0b
snowykte0426 <snowykte0426@naver.com>
2025-02-08 00:48:10 +0900

    init: 🎉Groom 프로젝트 시작합니다!
    Springboot 프로젝트와 README를 준비했습니다!

---
+++ build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.2"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.ampersand"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	/* Spring Boot Starter */
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-mail")
+
+	/** AOP */
+	implementation("
+	implementation("org.springframework.boot:spring-boot-starter-aop")
+
+    /** Lombok */
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+
+	/** Database */
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	runtimeOnly("com.mysql:mysql-connector-j")
+
+	/** Security */
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
