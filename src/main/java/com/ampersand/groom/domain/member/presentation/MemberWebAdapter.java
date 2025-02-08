cea9ec2d1b787130235b8640ec630f1ef2eb83fa
snowykte0426 <snowykte0426@naver.com>
2025-02-08 22:59:43 +0900

    add: ``Member`` 컨트롤러 클래스 선언
    Member의 컨트롤러 클래스를 선언하였습니다

---
+++ src/main/java/com/ampersand/groom/domain/member/presentation/MemberWebAdapter.java
@@ -0,0 +1,12 @@
+package com.ampersand.groom.domain.member.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/members")
+public class MemberWebAdapter {
+
+}
