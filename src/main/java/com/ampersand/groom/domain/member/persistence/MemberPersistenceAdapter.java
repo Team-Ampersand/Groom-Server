248f9d90a00dd79b87d4215cdacf54d54f1839b8
snowykte0426 <snowykte0426@naver.com>
2025-02-11 19:27:02 +0900

    add: Member Persistence Adapter 구현
    Persistence 계층으로 접근하는 Adapter를 구현하였습니다

---
+++ src/main/java/com/ampersand/groom/domain/member/persistence/MemberPersistenceAdapter.java
@@ -0,0 +1,48 @@
+package com.ampersand.groom.domain.member.persistence;
+
+import com.ampersand.groom.domain.member.application.port.MemberPersistenceReadPort;
+import com.ampersand.groom.domain.member.application.port.MemberPersistenceWritePort;
+import com.ampersand.groom.domain.member.domain.Member;
+import com.ampersand.groom.domain.member.domain.constant.MemberRole;
+import com.ampersand.groom.domain.member.exception.MemberNotFoundException;
+import com.ampersand.groom.domain.member.persistence.mapper.MemberMapper;
+import com.ampersand.groom.domain.member.persistence.repository.MemberJpaRepository;
+import com.ampersand.groom.global.annotation.adapter.Adapter;
+import com.ampersand.groom.global.annotation.adapter.constant.AdapterType;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Adapter(AdapterType.OUTBOUND)
+@RequiredArgsConstructor
+public class MemberPersistenceAdapter implements MemberPersistenceReadPort, MemberPersistenceWritePort {
+
+    private final MemberJpaRepository memberJpaRepository;
+    private final MemberMapper memberMapper;
+
+    @Override
+    public List<Member> queryAllMember() {
+        return memberJpaRepository.findAll().stream()
+                .map(memberMapper::toDomain)
+                .toList();
+    }
+
+    @Override
+    public List<Member> searchMember(Long id, String name, Integer generation, String email, Boolean isAvailable, MemberRole role) {
+        return memberJpaRepository.searchMember(id, name, generation, email, isAvailable, role).stream()
+                .map(memberMapper::toDomain)
+                .toList();
+    }
+
+    @Override
+    public Member queryMember(Long id) {
+        return memberJpaRepository.findById(id)
+                .map(memberMapper::toDomain)
+                .orElseThrow(MemberNotFoundException::new);
+    }
+
+    @Override
+    public void updateMemberPassword(Long id, String newPassword) {
+        memberJpaRepository.updatePassword(id, newPassword);
+    }
+}
