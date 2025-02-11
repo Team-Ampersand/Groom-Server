537ac7fba06e876293d8993368b4b8503d5e2fa1
snowykte0426 <snowykte0426@naver.com>
2025-02-11 19:24:39 +0900

    add: ``toResponse`` 메서드 추가
    Member 객체를 받아 GetMemberResponse 객체로 변환해주는 메서드를 추가하였습니다

--- src/main/java/com/ampersand/groom/domain/member/persistence/mapper/MemberMapper.java
+++ src/main/java/com/ampersand/groom/domain/member/persistence/mapper/MemberMapper.java
@@ -2,8 +2,11 @@ package com.ampersand.groom.domain.member.persistence.mapper;
 
 import com.ampersand.groom.domain.member.domain.Member;
 import com.ampersand.groom.domain.member.persistence.entity.MemberJpaEntity;
+import com.ampersand.groom.domain.member.presentation.data.response.GetMemberResponse;
 import com.ampersand.groom.global.mapper.GenericMapper;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MemberMapper implements GenericMapper<MemberJpaEntity, Member> {
     @Override
     public MemberJpaEntity toEntity(Member member) {
@@ -30,4 +33,17 @@ public class MemberMapper implements GenericMapper<MemberJpaEntity, Member> {
                 .role(jpaEntity.getRole())
                 .build();
     }
+
+    public GetMemberResponse toResponse(Member member) {
+        return new GetMemberResponse(
+                member.getId(),
+                member.getName(),
+                member.getGeneration(),
+                member.getEmail(),
+                member.getIsAvailable(),
+                member.getRole()
+        );
+    }
+
+    // TODO: booking 구현 후 current member api 구현 시 오버로딩 하여 toResponse 메서드 하나 더 구현해야 할것 같습니다
 }
