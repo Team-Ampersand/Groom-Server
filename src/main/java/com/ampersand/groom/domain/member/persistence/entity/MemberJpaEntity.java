4ce1874f65d448b6e28f342c367996511d52e33c
snowykte0426 <snowykte0426@naver.com>
2025-02-11 15:38:37 +0900

    update: Auto Increment 기반 PK로 변경
    UUID 기반 PK에서 정수형으로 자동 증분되는 형식의 PK로 변경하였습니다

--- src/main/java/com/ampersand/groom/domain/member/persistence/entity/MemberJpaEntity.java
+++ src/main/java/com/ampersand/groom/domain/member/persistence/entity/MemberJpaEntity.java
@@ -1,21 +1,19 @@
 package com.ampersand.groom.domain.member.persistence.entity;
 
 import com.ampersand.groom.domain.member.domain.constant.MemberRole;
-import com.ampersand.groom.global.entity.BaseUuidEntity;
+import com.ampersand.groom.global.entity.BaseIdEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import java.util.UUID;
-
 @Getter
 @NoArgsConstructor
 @Entity
 @Table(name = "member")
 @ToString
-public class MemberJpaEntity extends BaseUuidEntity {
+public class MemberJpaEntity extends BaseIdEntity {
     @Column(nullable = false)
     private String name;
     @Column(nullable = false, unique = true)
@@ -32,7 +30,7 @@ public class MemberJpaEntity extends BaseUuidEntity {
     private MemberRole role;
 
     @Builder
-    public MemberJpaEntity(UUID id, String name, String email, String password, Integer generation, Boolean isAvailable, MemberRole role) {
+    public MemberJpaEntity(Long id, String name, String email, String password, Integer generation, Boolean isAvailable, MemberRole role) {
         this.id = id;
         this.name = name;
         this.email = email;
