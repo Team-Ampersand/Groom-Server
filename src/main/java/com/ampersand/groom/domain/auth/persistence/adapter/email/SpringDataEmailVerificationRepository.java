f02ecdb920245d3ae0b4344375e9c9739bdd8e81
se0hu1 <leesh6541@gmail.com>
2025-02-14 02:31:59 +0900

    add: Spring Data 기반 EmailVerificationRepository 추가

---
+++ src/main/java/com/ampersand/groom/domain/auth/persistence/adapter/email/SpringDataEmailVerificationRepository.java
@@ -0,0 +1,25 @@
+package com.ampersand.groom.domain.auth.persistence.adapter.email;
+
+import com.ampersand.groom.domain.auth.domain.model.EmailVerification;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Repository
+public interface SpringDataEmailVerificationRepository extends JpaRepository<EmailVerification, Long> {
+
+    Optional<EmailVerification> findByCode(String code);
+
+    Optional<EmailVerification> findByEmail(String email);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM EmailVerification e WHERE e.verificationDate < :now")
+    void deleteAllExpired(@Param("now") LocalDateTime now);
+}
