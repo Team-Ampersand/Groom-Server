b2f0b142baec8fd551e7dc570e33d957d8e18f4d
se0hu1 <leesh6541@gmail.com>
2025-02-14 15:33:08 +0900

    update: 이메일 인증 엔티티 생성 및 테이블 이름을 'email'로 지정

--- src/main/java/com/ampersand/groom/domain/auth/domain/model/EmailVerification.java
+++ src/main/java/com/ampersand/groom/domain/auth/domain/model/EmailVerification.java
@@ -1,27 +1,28 @@
 package com.ampersand.groom.domain.auth.domain.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "email")
+@ToString
 public class EmailVerification {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(nullable = false)
     private String email;
+    @Column(nullable = false)
     private String code;
+    @Column(nullable = false)
     private boolean isVerified;
+    @Column(nullable = false)
     private LocalDateTime verificationDate;
 
     public EmailVerification(String email, String code) {
@@ -29,6 +30,15 @@ public class EmailVerification {
         this.code = code;
         this.isVerified = false;
         this.verificationDate = LocalDateTime.now().plusMinutes(5);
+    }
+
+    @Builder
+    public EmailVerification(Long id, String email, String code, boolean isVerified, LocalDateTime verificationDate) {
+        this.id = id;
+        this.email = email;
+        this.code = code;
+        this.isVerified = isVerified;
+        this.verificationDate = verificationDate;
     }
 
 
