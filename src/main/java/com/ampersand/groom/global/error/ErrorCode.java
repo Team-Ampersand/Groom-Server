e49d0e0374ffa6cd04ea067828adda7fea445f73
se0hu1 <leesh6541@gmail.com>
2025-02-15 22:23:04 +0900

    add: 오류 추가

--- src/main/java/com/ampersand/groom/global/error/ErrorCode.java
+++ src/main/java/com/ampersand/groom/global/error/ErrorCode.java
@@ -8,7 +8,9 @@ import lombok.Getter;
 public enum ErrorCode {
 
     MEMBER_ALREADY_EXISTS("Member already exists", 409),
-    MEMBER_NOT_FOUND("Member not found", 404);
+    MEMBER_NOT_FOUND("Member not found", 404),
+    INVALID_OR_EXPIRED("Invalid or expired", 401),
+    INVALID_FORMAT("Invalid format", 400);
 
     private final String message;
     private final int httpStatus;
