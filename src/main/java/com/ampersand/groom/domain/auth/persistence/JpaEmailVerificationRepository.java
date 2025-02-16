78070cfe8956b46a345310c5c385301060185346
se0hu1 <leesh6541@gmail.com>
2025-02-16 13:05:06 +0900

    update: port 상속 제거

--- src/main/java/com/ampersand/groom/domain/auth/persistence/JpaEmailVerificationRepository.java
+++ src/main/java/com/ampersand/groom/domain/auth/persistence/JpaEmailVerificationRepository.java
@@ -1,38 +1,31 @@
-package com.ampersand.groom.domain.auth.persistence.adapter;
+package com.ampersand.groom.domain.auth.persistence;
 
-import com.ampersand.groom.domain.auth.application.port.EmailVerificationPort;
-import com.ampersand.groom.domain.auth.domain.model.EmailVerification;
 import com.ampersand.groom.domain.auth.persistence.adapter.email.SpringDataEmailVerificationRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
-public class JpaEmailVerificationRepository implements EmailVerificationPort {
+@RequiredArgsConstructor
+public class JpaEmailVerificationRepository {
 
     private final SpringDataEmailVerificationRepository repository;
 
-    public JpaEmailVerificationRepository(SpringDataEmailVerificationRepository repository) {
-        this.repository = repository;
-    }
 
-    @Override
     public EmailVerification save(EmailVerification emailVerification) {
         return repository.save(emailVerification);
     }
 
-    @Override
     public Optional<EmailVerification> findByCode(String code) {
         return repository.findByCode(code);
     }
 
-    @Override
     public Optional<EmailVerification> findByEmail(String email) {
         return repository.findByEmail(email);
     }
 
-    @Override
     public void deleteAllExpired(LocalDateTime now) {
         repository.deleteAllExpired(now);
     }
