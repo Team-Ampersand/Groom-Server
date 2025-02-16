8fb525f4d874a8e8084e825aea4387b8e18d0c60
se0hu1 <leesh6541@gmail.com>
2025-02-16 12:33:33 +0900

    delete: AuthException 삭제

--- src/main/java/com/ampersand/groom/domain/auth/expection/AuthException.java
+++
@@ -1,7 +0,0 @@
-package com.ampersand.groom.domain.auth.expection;
-
-public class AuthException extends RuntimeException {
-    public AuthException(String message) {
-        super(message);
-    }
-}
