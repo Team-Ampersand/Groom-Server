13f9762503f1ff1fe67e4a62159602b5727e15cf
se0hu1 <leesh6541@gmail.com>
2025-02-17 14:28:28 +0900

    chore: ErrorCode

--- src/main/java/com/ampersand/groom/global/error/ErrorCode.java
+++ src/main/java/com/ampersand/groom/global/error/ErrorCode.java
@@ -9,7 +9,7 @@ public enum ErrorCode {
 
     MEMBER_ALREADY_EXISTS("Member already exists", 409),
     MEMBER_NOT_FOUND("Member not found", 404),
-    VERIFICATION_CODE_EXPIRED_OR_INVALID("Verification code expired or Invalid", 401),
+    VERIFICATION_CODE_EXPIRED_OR_INVALID("Verification code expired or invalid", 401),
     EMAIL_FORMAT_INVALID("Email format invalid", 400),
     VERIFICATION_CODE_FORMAT_INVALID("Verification code format invalid", 400);
 
