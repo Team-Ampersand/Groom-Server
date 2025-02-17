9571324f3ea39829294b9390eb94afec1d7606d6
se0hu1 <leesh6541@gmail.com>
2025-02-17 14:25:40 +0900

    add: 예외 명칭 세분화

--- src/main/java/com/ampersand/groom/global/error/ErrorCode.java
+++ src/main/java/com/ampersand/groom/global/error/ErrorCode.java
@@ -9,8 +9,9 @@ public enum ErrorCode {
 
     MEMBER_ALREADY_EXISTS("Member already exists", 409),
     MEMBER_NOT_FOUND("Member not found", 404),
-    INVALID_OR_EXPIRED("Invalid or expired", 401),
-    INVALID_FORMAT("Invalid format", 400);
+    VERIFICATION_CODE_EXPIRED_OR_INVALID("Verification code expired or Invalid", 401),
+    EMAIL_FORMAT_INVALID("Email format invalid", 400),
+    VERIFICATION_CODE_FORMAT_INVALID("Verification code format invalid", 400);
 
     private final String message;
     private final int httpStatus;
