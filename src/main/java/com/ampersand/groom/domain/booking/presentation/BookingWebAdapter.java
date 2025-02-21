b74ba192e59ed5b50612e292f695efcae2d0271d
KIM TAEEUN <snowykte0426@naver.com>
2025-02-22 03:16:59 +0900

    update: ``Booking`` 엔드포인트 변경
    API 명세서와 다르게 설계되었던 엔드포인트를 문서에 맞도록 수정하였습니다

--- src/main/java/com/ampersand/groom/domain/booking/presentation/BookingWebAdapter.java
+++ src/main/java/com/ampersand/groom/domain/booking/presentation/BookingWebAdapter.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
-@RequestMapping("/api/v1/bookings")
+@RequestMapping("/api/v1/booking")
 @RestController
 @RequiredArgsConstructor
 public class BookingWebAdapter {
