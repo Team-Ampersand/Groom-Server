32fccd2f04c424df9a083ed79e3770d1f32e4de1
KIM TAEEUN <snowykte0426@naver.com>
2025-02-22 03:12:04 +0900

    add: Place 영속 계층 Adapter 선언 맟 정의
    Place의 영속 계층에서 사용될 Adapter를 선언하였습니다

---
+++ src/main/java/com/ampersand/groom/domain/booking/persistence/PlacePersistenceAdapter.java
@@ -0,0 +1,27 @@
+package com.ampersand.groom.domain.booking.persistence;
+
+import com.ampersand.groom.domain.booking.application.port.PlacePersistencePort;
+import com.ampersand.groom.domain.booking.domain.Place;
+import com.ampersand.groom.domain.booking.persistence.mapper.PlaceMapper;
+import com.ampersand.groom.domain.booking.persistence.repository.PlaceJpaRepository;
+import com.ampersand.groom.global.annotation.adapter.Adapter;
+import com.ampersand.groom.global.annotation.adapter.constant.AdapterType;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Adapter(AdapterType.OUTBOUND)
+public class PlacePersistenceAdapter implements PlacePersistencePort {
+
+    private final PlaceJpaRepository placeJpaRepository;
+    private final PlaceMapper placeMapper;
+
+    @Override
+    public List<Place> findPlaceByBookingAvailability(LocalDate date, String time, String placeType) {
+        return placeJpaRepository.findPlaceByBookingAvailability(date, time, placeType).stream()
+                .map(placeMapper::toDomain)
+                .toList();
+    }
+}
