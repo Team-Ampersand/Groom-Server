b6112ce78557ef81b4b5825344a3188453ddd5b1
KIM TAEEUN <snowykte0426@naver.com>
2025-02-24 02:34:44 +0900

    update: ``president`` 필드 추가
    대표자 정보를 포함하는 필드를 요구 사항의 변화에 따라 추가하였습니다

--- src/main/java/com/ampersand/groom/domain/booking/presentation/data/response/GetBookingResponse.java
+++ src/main/java/com/ampersand/groom/domain/booking/presentation/data/response/GetBookingResponse.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public record GetBookingResponse(
         String place,
+        Member president,
         List<Member> participants,
         LocalDate date,
         String time
