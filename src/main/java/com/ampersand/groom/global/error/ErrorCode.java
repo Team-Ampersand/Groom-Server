7d9197a33c0829afe82ee329d7073a219bb8dd63
KIM TAEEUN <snowykte0426@naver.com>
2025-02-24 20:38:16 +0900

    add: 예약 생성 API 실행 시 발생 가능 예외 상황 정의
    올바르지 않은 시간대,장소로 예약이 요청되었거나 중복된 예약이 생성요청 되었을 경우 등 다양한 예외 상황을 정의하였습니다

--- src/main/java/com/ampersand/groom/global/error/ErrorCode.java
+++ src/main/java/com/ampersand/groom/global/error/ErrorCode.java
@@ -7,8 +7,19 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
+    // MEMBER
     MEMBER_ALREADY_EXISTS("Member already exists", 409),
     MEMBER_NOT_FOUND("Member not found", 404),
+
+    // BOOKING
+    INVALID_BOOKING_TIME("Invalid booking time", 400),
+    INVALID_BOOKING_PARTICIPANTS("Invalid booking participants", 400),
+    DUPLICATE_BOOKING("Duplicate booking", 409),
+
+    // PLACE
+    PLACE_NOT_FOUND("Place not found", 404),
+
+    // EMAIL AUTHENTICATION
     VERIFICATION_CODE_EXPIRED_OR_INVALID("Verification code expired or invalid", 401),
     EMAIL_FORMAT_INVALID("Email format invalid", 400),
     VERIFICATION_CODE_FORMAT_INVALID("Verification code format invalid", 400);
