da518e9228a62b5817efe78f81cdf424c2266f1a
KIM TAEEUN <snowykte0426@naver.com>
2025-02-24 18:24:26 +0900

    update: ``FindBookingByDAteAndTimeAndPlaceUseCase`` 구현
    실제로 미구현 되어있던 클래스를 구현하였습니다

--- src/main/java/com/ampersand/groom/domain/booking/application/usecase/FindBookingByDateAndTimeAndPlaceUseCase.java
+++ src/main/java/com/ampersand/groom/domain/booking/application/usecase/FindBookingByDateAndTimeAndPlaceUseCase.java
@@ -1,6 +1,7 @@
 package com.ampersand.groom.domain.booking.application.usecase;
 
 import com.ampersand.groom.domain.booking.application.BookingApplicationAdapter;
+import com.ampersand.groom.domain.booking.persistence.BookingPersistenceAdapter;
 import com.ampersand.groom.domain.booking.persistence.mapper.BookingMapper;
 import com.ampersand.groom.domain.booking.presentation.data.response.GetBookingResponse;
 import com.ampersand.groom.global.annotation.usecase.UseCaseWithReadOnlyTransaction;
@@ -13,13 +14,12 @@ import java.util.List;
 @UseCaseWithReadOnlyTransaction
 public class FindBookingByDateAndTimeAndPlaceUseCase {
 
-    private final BookingApplicationAdapter booking // PersistenceAdapter로 변경
+    private final BookingPersistenceAdapter bookingPersistenceAdapter;
     private final BookingMapper bookingMapper;
 
     public List<GetBookingResponse> execute(LocalDate date, String time, String place) {
-/*        return bookingApplicationAdapter.findBookingByDateAndTimeAndPlace(date, time, place).stream()
+        return bookingPersistenceAdapter.findBookingByDateAndTimeAndPlace(date, time, place).stream()
                 .map(bookingMapper::toResponse)
-                .toList();*/
-        return List.of();
+                .toList();
     }
 }
