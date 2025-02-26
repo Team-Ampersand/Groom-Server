de4594dc7dd4ea24cda927e7d548b3eb153af564
se0hu1 <leesh6541@gmail.com>
2025-02-27 01:09:31 +0900

    add: JWT 인증 필터 추가

---
+++ src/main/java/com/ampersand/groom/global/security/config/JwtAuthenticationFilter.java
@@ -0,0 +1,38 @@
+package com.ampersand.groom.global.security.config;
+
+import com.ampersand.groom.domain.auth.application.service.CustomUserDetailsService;
+import com.ampersand.groom.domain.auth.application.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String token = jwtService.resolveToken(request);
+
+        if (token != null && jwtService.validateToken(token)) {
+            String email = jwtService.getEmailFromToken(token);
+            UserDetails userDetails = customUserDetailsService.loadUserByUsername(email);
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
