4758692f8191493ea10b23cfc42b12848cc96b95
KIM TAEEUN <snowykte0426@naver.com>
2025-03-08 20:50:05 +0900

    fix: 예약 삭제 로직에서 대표자 이메일 확인 조건 수정
    참이여야 하는 상황에서 조건이 올바르지 않아 예외를 발행하던 문제를 수정하였습니다

--- src/main/java/com/ampersand/groom/domain/booking/application/usecase/DeleteBookingUseCase.java
+++ src/main/java/com/ampersand/groom/domain/booking/application/usecase/DeleteBookingUseCase.java
@@ -17,7 +17,7 @@ public class DeleteBookingUseCase {
 
     public void execute(Long bookingId) {
         Booking booking = bookingPersistencePort.findBookingByIdWithLock(bookingId);
-        if (booking.getPresident().getEmail().equals(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString())) {
+        if (!booking.getPresident().getEmail().equals(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString())) {
             throw new NotBookingPresidentException();
         }
         bookingPersistencePort.deleteBookingById(bookingId);
