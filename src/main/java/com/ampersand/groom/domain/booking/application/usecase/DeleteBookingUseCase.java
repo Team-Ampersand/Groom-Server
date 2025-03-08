1f49f8c5bb0525ba3eced3d482a5aa0243307b60
KIM TAEEUN <snowykte0426@naver.com>
2025-03-08 20:36:46 +0900

    add: 예약 삭제 비즈니스 로직 추가
    예약 삭제를 진행하는 Use Case 클래스를 추가하였습니다

---
+++ src/main/java/com/ampersand/groom/domain/booking/application/usecase/DeleteBookingUseCase.java
@@ -0,0 +1,25 @@
+package com.ampersand.groom.domain.booking.application.usecase;
+
+import com.ampersand.groom.domain.booking.application.port.BookingPersistencePort;
+import com.ampersand.groom.domain.booking.domain.Booking;
+import com.ampersand.groom.domain.booking.exception.NotBookingPresidentException;
+import com.ampersand.groom.domain.member.application.port.MemberPersistencePort;
+import com.ampersand.groom.global.annotation.usecase.UseCaseWithTransaction;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@UseCaseWithTransaction
+@RequiredArgsConstructor
+public class DeleteBookingUseCase {
+
+    private final BookingPersistencePort bookingPersistencePort;
+    private final MemberPersistencePort memberPersistencePort;
+
+    public void execute(Long bookingId) {
+        Booking booking = bookingPersistencePort.findBookingByIdWithLock(bookingId);
+        if(booking.getPresident().getEmail().equals(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString())) {
+            throw new NotBookingPresidentException();
+        }
+        bookingPersistencePort.deleteBooking(booking);
+    }
+}
