7c1bacb9bb9614307cfac27f2aab230a089ba010
KIM TAEEUN <snowykte0426@naver.com>
2025-03-28 09:56:47 +0900

    add: ``MessagingException`` 예외 핸들링 메서드 추가
    이메일 전송 실패 시 반환할 정보를 구성하는 메서드를 추가 구현하였습니다

--- src/main/java/com/ampersand/groom/global/error/handler/GlobalExceptionHandler.java
+++ src/main/java/com/ampersand/groom/global/error/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.ampersand.groom.global.error.handler;
 
 import com.ampersand.groom.global.error.data.response.ErrorResponse;
 import com.ampersand.groom.global.error.exception.GroomException;
+import jakarta.mail.MessagingException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,5 +15,12 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(e.getErrorCode().getHttpStatus())
                 .body(new ErrorResponse(e.getErrorCode().getMessage(), e.getErrorCode().getHttpStatus()));
+    }
+
+    @ExceptionHandler(MessagingException.class)
+    public ResponseEntity<ErrorResponse> handleMessagingException(MessagingException e) {
+        return ResponseEntity
+                .status(500)
+                .body(new ErrorResponse("Failed to send email.", 500));
     }
 }
